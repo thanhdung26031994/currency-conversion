72a27502d9a102a842cfe58a74fb8f9ee7fb646d
thanhdung26031994 <thanhdung26031994@gmail.com>
2024-02-26 22:51:47 +0700

    echo

--- src/main/java/org/example/currencyconversion/CurrencyController.java
+++ src/main/java/org/example/currencyconversion/CurrencyController.java
@@ -1,2 +1,22 @@
-package org.example.currencyconversion;public class CurrencyController {
+package org.example.currencyconversion;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CurrencyController {
+    @GetMapping ("/home")
+    public String showCurrency(){
+        return "home";
+    }
+    @PostMapping("/home")
+    public ModelAndView convert(@RequestParam("usd") float usd, @RequestParam("rate") float rate){
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("usd", usd);
+        modelAndView.addObject("result", usd * rate);
+        return modelAndView;
+    }
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -7,10 +7,23 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+    <title>Ứng dụng chuyển đổi tiền tệ</title>
+</head>
+<body>
+<form method="post">
+    <fieldset>
+        <legend>
+            Currency Convert - Chuyển đổi tiền tệ
+        </legend>
+        <label for="USD" style="display: block;">Nhập USD</label>
+        <input type="number" name="usd" id="USD"><br>
+        <label for="rate" style="display: block">Tỉ giá</label>
+        <input name="rate" type="number" value="24000" id="rate"><br>
+        <input type="submit" value="Convert"><br>
+        <p>USD: ${usd}</p>
+        <p>VND: ${result}</p>
+    </fieldset>
+</form>
+</body>
 </html>
